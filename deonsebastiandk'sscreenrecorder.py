bdc224168ec998500f4d9450ced50645d5e800ef
deonsebastiandk <71586610+deonsebastiandk@users.noreply.github.com>
2021-01-14 07:17:19 +0530

    Update deonsebastiandk'sscreenrecorder.py

--- deonsebastiandk'sscreenrecorder.py
+++ deonsebastiandk'sscreenrecorder.py
@@ -1,15 +1,15 @@
-import cv2
-import numpy as np
+Import cv2
+Import numpy As np
 
 screen_size=(1920,1080)
 fourcc=cv2.VideoWriter_fourcc(*"XVID")
 out=cv2.VideoWriter("dsdkosscreenrecordervideo.mp4,fourcc,20.0,(screen_size))
 
-while True:
+While True:
 img=pyautogui.screenshot()
 frame=np.array(img)
 frame=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
 out.write(frame)
 cv2.imshow("show",frame)
 if cv2.waitKey(1)==ord("q") :
-break
+Break
